f016bcb6b18d93073979965b3f580184da76751e
仲宗根悠太 <nakasoneyuuta@nakasoneyuutanoMacBook-Air.local>
2021-01-18 16:06:56 +0900

    removed Player class and Bot class

--- app/src/main/java/jp/ac/uryukyu/ie/e205757/Bot.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.uryukyu.ie.e205757;
-import java.util.ArrayList;
-
-public class Bot extends Character{
-    Bot(ArrayList<Integer> cards){
-        super(cards);
-    }
-}
--- app/src/main/java/jp/ac/uryukyu/ie/e205757/Player.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.uryukyu.ie.e205757;
-import java.util.ArrayList;
-
-public class Player extends Character{
-    Player(ArrayList<Integer> cards){
-        super(cards);
-    }
-}
